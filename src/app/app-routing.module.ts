a0e598073164d5c3f937c99fc3b5341396ba1d7b
tronghenry273 <101923224+QuiBuuIzTheBest@users.noreply.github.com>
2023-09-12 14:11:48 +0700

    complete routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './contents/page-not-found/page-not-found.component';
-import { HomeComponent } from './contents/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { HistoryComponent } from './views/history/history.component';
+import { HistoryDetailComponent } from './views/history-detail/history-detail.component';
+import { LoginComponent } from './views/login/login.component';
+import { BlogComponent } from './views/blog/blog.component';
+import { BlogSingleComponent } from './views/blog-single/blog-single.component';
+import { DestinationComponent } from './views/destination/destination.component';
+import { DestinationDetailComponent } from './views/destination-detail/destination-detail.component';
+import { ForgotpassComponent } from './views/forgotpass/forgotpass.component';
+import { SignUpComponent } from './views/sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: 'history-detail', component: HistoryDetailComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'blog-single', component: BlogSingleComponent},
+  {path: 'destination', component: DestinationComponent},
+  {path: 'destination-detail', component: DestinationDetailComponent},
+  {path: 'forgot-pass', component: ForgotpassComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'login', component: LoginComponent},
   {path: '', component: HomeComponent},
   {path: '**', component: PageNotFoundComponent},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './contents/home/home.component';
-import { PageNotFoundComponent } from './contents/page-not-found/page-not-found.component';
+import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { HistoryComponent } from './views/history/history.component';
+import { HistoryDetailComponent } from './views/history-detail/history-detail.component';
+import { LoginComponent } from './views/login/login.component';
+import { BlogSingleComponent } from './views/blog-single/blog-single.component';
+import { BlogComponent } from './views/blog/blog.component';
+import { DestinationComponent } from './views/destination/destination.component';
+import { DestinationDetailComponent } from './views/destination-detail/destination-detail.component';
+import { ForgotpassComponent } from './views/forgotpass/forgotpass.component';
+import { SignUpComponent } from './views/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +25,18 @@ import { PageNotFoundComponent } from './contents/page-not-found/page-not-found.
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    AboutComponent,
+    ContactComponent,
+    HistoryComponent,
+    HistoryDetailComponent,
+    LoginComponent,
+    BlogSingleComponent,
+    BlogComponent,
+    DestinationComponent,
+    DestinationDetailComponent,
+    ForgotpassComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
